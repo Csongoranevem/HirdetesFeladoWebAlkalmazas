c5855208f2e0b8272da02eefa1044f11533814ce
Csongoranevem <czakocsongor@turr.hu>
2026-02-13 21:34:26 +0100

    HIR-24 "Új hirdetés" gomb elkészítése és animálása

--- HIR_frontend/src/app/components/system/header/header.component.ts
+++ HIR_frontend/src/app/components/system/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SearchbarComponent } from '../searchbar/searchbar.component';
+import { NewAdComponent } from '../new-ad/new-ad.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [SearchbarComponent],
+  imports: [SearchbarComponent, NewAdComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
