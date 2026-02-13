29522c1839d910cc39db32f0f96216187309bf88
Csongoranevem <czakocsongor@turr.hu>
2026-02-13 17:31:40 +0100

    HIR-23, HIR-27 fejléc komponens létrehozása, formázása, a fő logó elkészítése és elhelyezése az oldalon

---
+++ HIR_frontend/src/app/components/system/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SearchbarComponent } from '../searchbar/searchbar.component';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [SearchbarComponent],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
