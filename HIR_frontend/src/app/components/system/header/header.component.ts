e469974ee24c767d966067745ef1b4a282d6e7f2
Csongoranevem <czakocsongor@turr.hu>
2026-02-14 15:32:49 +0100

    HIR-25, HIR-26 súgó gomb és profil lenyíló menüjének elkészítése

--- HIR_frontend/src/app/components/system/header/header.component.ts
+++ HIR_frontend/src/app/components/system/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SearchbarComponent } from '../searchbar/searchbar.component';
 import { NewAdComponent } from '../new-ad/new-ad.component';
+import { ProfileSectionComponent } from '../profile-section/profile-section.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [SearchbarComponent, NewAdComponent],
+  imports: [SearchbarComponent, NewAdComponent, ProfileSectionComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
