e82da6a78d337755e9d5e5fbafdf70bcbbacbf1e
Zalan12 <farkaszalan@turr.hu>
2026-02-16 19:06:56 +0100

    HIR-12 Helytelen elérési út komponens implementálása

--- HIR_frontend/src/app/app.routes.ts
+++ HIR_frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { PagenotfoundComponent } from './components/system/pagenotfound/pagenotfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    
+    
+    //This route must be in the last line
+    {path: '**',component:PagenotfoundComponent}
+    
+];
