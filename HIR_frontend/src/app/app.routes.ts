31d242a17d2329877c404aa8b4673e5cedde0694
Zalan12 <farkaszalan@turr.hu>
2026-02-17 12:49:16 +0100

    HIR-33 Átállás primeNG-re

--- HIR_frontend/src/app/app.routes.ts
+++ HIR_frontend/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { PagenotfoundComponent } from './components/system/pagenotfound/pagenotfound.component';
 import { HomeComponent } from './components/system/home/home.component';
+import { RegistrationComponent } from './components/user/registration/registration.component';
 
 export const routes: Routes = [
     {path: 'home', component:HomeComponent},
-    
+    {path:'registration', component:RegistrationComponent},
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     //This route must be in the last line
     {path: '**',component:PagenotfoundComponent},
