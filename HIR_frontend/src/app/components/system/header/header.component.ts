2671ab45a8c4bd7a40f8313db0a168cc81f67e6d
Csongoranevem <czakocsongor@turr.hu>
2026-02-17 12:59:00 +0100

    HIR-33 provider beillesztése

--- HIR_frontend/src/app/components/system/header/header.component.ts
+++ HIR_frontend/src/app/components/system/header/header.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { ImageModule } from 'primeng/image';
+import { Toolbar } from 'primeng/toolbar';
+import { AvatarModule } from 'primeng/avatar';
+import { SharedModule } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ImageModule],
+  imports: [ImageModule, Toolbar, AvatarModule, SharedModule, ButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
