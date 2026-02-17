e26b4f2d1d3afed2a2a1ffee0c00214b78303e95
Csongoranevem <czakocsongor@turr.hu>
2026-02-17 13:23:57 +0100

    HIR-1 fejléc elkészítése

--- HIR_frontend/src/app/components/system/header/header.component.ts
+++ HIR_frontend/src/app/components/system/header/header.component.ts
@@ -4,13 +4,24 @@ import { Toolbar } from 'primeng/toolbar';
 import { AvatarModule } from 'primeng/avatar';
 import { SharedModule } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { AutoCompleteModule } from 'primeng/autocomplete';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ImageModule, Toolbar, AvatarModule, SharedModule, ButtonModule],
+  imports: [ImageModule, Toolbar, AvatarModule, SharedModule, ButtonModule, FormsModule, AutoCompleteModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  selectedItem: any;
+  filteredItems: any[] = [];
+
+  filterItems(event: any) {}
+
+  items: any[] = [];
+
+
 
 }
