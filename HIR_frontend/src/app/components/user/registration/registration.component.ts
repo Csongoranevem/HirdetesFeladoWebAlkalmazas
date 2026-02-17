e4fadb8481fac5f6a75434caceca266bd7f43dd4
Zalan12 <farkaszalan@turr.hu>
2026-02-17 12:59:39 +0100

    HIR-2 Lábléc komponens fixálása

--- HIR_frontend/src/app/components/user/registration/registration.component.ts
+++ HIR_frontend/src/app/components/user/registration/registration.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { FloatLabelModule } from "primeng/floatlabel"
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [],
+  imports: [FloatLabelModule,InputTextModule,FormsModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss'
 })
