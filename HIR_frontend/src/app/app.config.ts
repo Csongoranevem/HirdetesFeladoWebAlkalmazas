b9bda8c839ef45957fd3d73d797fe5828030445b
Zalan12 <farkaszalan@turr.hu>
2026-02-17 13:07:11 +0100

    HIR-12 Elérési utak komponens javítása

--- HIR_frontend/src/app/app.config.ts
+++ HIR_frontend/src/app/app.config.ts
@@ -1,13 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
+import { provideRouter } from '@angular/router';
 import Aura from '@primeng/themes/aura';
-
 import { routes } from './app.routes';
 
 
 export const appConfig: ApplicationConfig = {
     providers: [
+        provideRouter(routes),
         provideAnimationsAsync(),
         providePrimeNG({ 
             theme: {
