644dc65c5177359911ee3e798dc9aa087dae9c1c
PiciMadar <rekdavid@turr.hu>
2026-02-18 09:31:01 +0100

    HIR-5 Home oldal beégetésének elkezdése

--- HIR_frontend/src/app/components/system/home/home.component.ts
+++ HIR_frontend/src/app/components/system/home/home.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CardModule, ButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
