5c2225e1543b66728f3f923c0dc3709041470ae7
Zalan12 <farkaszalan@turr.hu>
2026-03-12 14:05:43 +0100

    HIR-47 Új hirdetés komponens alapjainak kialakítása

--- HIR_frontend/src/app/components/user/myads/myads.component.ts
+++ HIR_frontend/src/app/components/user/myads/myads.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardsComponent } from '../../system/cards/cards.component';
 
 @Component({
   selector: 'app-myads',
   standalone: true,
-  imports: [],
+  imports: [CardsComponent],
   templateUrl: './myads.component.html',
   styleUrl: './myads.component.scss'
 })
